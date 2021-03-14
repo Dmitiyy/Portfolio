1e6449afd6e08b981f4c73e84462e9b7c8e8e552
Dmitriy <dmitriyy311@gmail.com>
2021-03-14 09:41:40 +0200

    porjects sass

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import projects from './model';
-import './projects-media.sass';
 import './projects.sass';
+import './projects-media.sass';
 
 const Projects = () => {
     return (
